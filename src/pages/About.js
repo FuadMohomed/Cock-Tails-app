25f44a7de292976907aa71aa917ee7d44105c464
FuadMohomed <fuadmohomed45@gmail.com>
2022-12-31 05:18:43 -0500

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const About = () => {
+  return (
+   <section className='section about-section'>
+     <h1 className='section-title'> about us</h1>
+     <p>jkdncdnkcnsd,mcnsdkncm,dnksskjcnsdndckjsdn
+       dc,dsnclkdsmclksdmclkdsmclkdmslsmcfhbvs,ndcjkdsnckjd
+       dklcnsdlkmcldkmclsmldkmlkcmldkmcldkmclkdmdclkdmjvndkmnvkls
+       dlkmcdlksmclksdmnkjfnv jfjfk fd vjkf jkf jkfj
+     </p>
+   </section>
+  )
+}
+
+export default About
